a63f1661a825d694d671f155312f562ba94bb1ec
Yasser M. Abdalkader <m.abdelkader.yasser@gmail.com>
2023-11-25 23:56:58 +0200

    Makefiles: created build.bat drv & build.bat clean

--- build.bat
+++ build.bat
@@ -1,7 +1,14 @@
 @echo off
-echo Building Hello, Wolrd!
+set "make=Make\make-3.81-bin\bin\make.exe"
+set "makefile=Make\makefile"
 REM Replace the following line with the actual command to run your program
-echo make.exe
-"Make\make-3.81-bin\bin\make.exe" --directory=Make
+if %1==clean goto clean
+if %1==all %make% -f %makefile% %1 >nul
+if %1==drv %make% -f %makefile% %1 >nul
+if %1=="" echo Please select the target
+:clean
+if exist Out (
+	%make% -f %makefile% %1 >nul
+)
+:end
 
-pause
