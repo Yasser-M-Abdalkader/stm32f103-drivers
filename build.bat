872a4f6bc97f7d87544526fac1e0fe022bade0c7
Yasser M. Abdalkader <m.abdelkader.yasser@gmail.com>
2023-11-25 14:22:59 +0200

    trial makefiles

---
+++ build.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Building Hello, Wolrd!
+REM Replace the following line with the actual command to run your program
+echo make.exe
+"Make\make-3.81-bin\bin\make.exe" --directory=Make
+
+pause
